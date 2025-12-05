2b49af2b7854b7fe41a43a22da7b7e3588740bcc
takakomaki <takako.maki@gmail.com>
2025-12-05 03:15:11 +0000

    Refactor code structure for improved readability and maintainability

---
+++ .next/dev/static/chunks/app_page_tsx_80d6446b._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/components_1f49e725._.js",
+  "static/chunks/node_modules_886cbb0b._.js"
+],
+    source: "dynamic"
+});
